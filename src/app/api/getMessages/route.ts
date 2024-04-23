90bb0862da3584bdbfb39dfd000161a5f0903aae
Arjun Khanna <arjunkhanna0108@gmail.com>
2024-04-24 02:28:05 +0530

    feat: moved to gemini

--- src/app/api/getMessages/route.ts
+++ src/app/api/getMessages/route.ts
@@ -4,35 +4,36 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-	const requestUrl = new URL(request.url);
-	const chatID = requestUrl.searchParams.get("chatID");
-	chatID?.toString();
-	const cookieStore = cookies();
-	const supabase = createServerComponentClient({ cookies });
-	const { data } = await supabase.auth.getSession();
+  const requestUrl = new URL(request.url);
+  const chatID = requestUrl.searchParams.get("chatID");
+  chatID?.toString();
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
 
-	if (!data.session?.user.id) {
-		return NextResponse.json([
-			{
-				error: "Internal Server Error",
-			},
-			{
-				status: 500,
-			},
-		]);
-	} else {
-		const userChats = await prisma.messages.findMany({
-			where: {
-				chatID: chatID!,
-			},
-		});
-		return NextResponse.json([
-			{
-				data: userChats,
-			},
-			{
-				status: 200,
-			},
-		]);
-	}
+  if (!data.session?.user.id) {
+    return NextResponse.json([
+      {
+        error: "Internal Server Error",
+      },
+      {
+        status: 500,
+      },
+    ]);
+  } else {
+    const userChats = await prisma.messages.findMany({
+      where: {
+        chatID: chatID!,
+      },
+    });
+    console.log(userChats);
+    return NextResponse.json([
+      {
+        data: userChats,
+      },
+      {
+        status: 200,
+      },
+    ]);
+  }
 }
--- src/app/chat/messageInterface.tsx
+++ src/app/chat/messageInterface.tsx
@@ -2,116 +2,105 @@ import React, { useEffect, useRef, useState } from "react";
 import Loader from "./loader";
 
 type messPropsType = {
-	chatID: string;
+  chatID: string;
 };
 
 export default function MessageInterface(this: any, props: messPropsType) {
-	console.log("message ran");
-	const [messgs, setMessgs] = useState<any[]>([]);
-	const [inputPrompt, setInputPrompt] = useState<string>("");
-	const [isLoading, setLoading] = useState<any>(true);
+  console.log("message ran");
+  const [messgs, setMessgs] = useState<any[]>([]);
+  const [inputPrompt, setInputPrompt] = useState<string>("");
+  const [isLoading, setLoading] = useState<any>(true);
 
-	useEffect(() => {
-		fetch("/api/getMessages?chatID=" + props.chatID, {
-			cache: "force-cache",
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				setMessgs(data[0].data);
+  useEffect(() => {
+    fetch("/api/getMessages?chatID=" + props.chatID)
+      .then((response) => response.json())
+      .then((data) => {
+        setMessgs(data[0].data);
 
-				let chat = document.getElementById("chat");
-				if (chat !== null) {
-					chat.scrollTop = chat.scrollHeight;
-				}
-				setLoading(false);
-			});
-	}, [isLoading === true]);
+        let chat = document.getElementById("chat");
+        if (chat !== null) {
+          chat.scrollTop = chat.scrollHeight;
+        }
+        setLoading(false);
+      });
+  }, [isLoading === true]);
 
-	if (isLoading === true || isLoading === null)
-		return (
-			<div className="w-full h-[100vh]">
-				<Loader />
-			</div>
-		);
+  if (isLoading === true || isLoading === null)
+    return (
+      <div className="w-full h-[100vh]">
+        <Loader />
+      </div>
+    );
 
-	async function getGPTresponse(
-		event: React.FormEvent
-	): Promise<React.FormEventHandler<HTMLFormElement> | undefined> {
-		setLoading(null);
-		const response = await fetch(
-			"/api/openai?prompt=" + inputPrompt + "&chatID=" + props.chatID,
-			{
-				method: "POST",
-				body: JSON.stringify({}),
-			}
-		);
-		if (response.status === 200) {
-			setLoading(true);
-			setMessgs([]);
-			// setLoading(false);
-		}
-		return;
-	}
+  async function getGPTresponse(
+    event: React.FormEvent
+  ): Promise<React.FormEventHandler<HTMLFormElement> | undefined> {
+    setLoading(null);
+    const response = await fetch(
+      "/api/openai?prompt=" + inputPrompt + "&chatID=" + props.chatID,
+      {
+        method: "POST",
+        body: JSON.stringify({}),
+      }
+    );
+    if (response.status === 200) {
+      setLoading(true);
+      setMessgs([]);
+      // setLoading(false);
+    }
+    return;
+  }
 
-	return (
-		<div>
-			<div
-				id="chat"
-				className="bg-gray-100 w-full h-[90vh] overflow-y-scroll"
-			>
-				<div className="w-[70%] ml-[10%] mt-8 space-y-4 mb-[50vh]">
-					{messgs.map(
-						(element: {
-							id: string;
-							query: string;
-							response: string;
-						}) => {
-							return (
-								<div key={element.id}>
-									<div>
-										<h3 className="font-bold">You</h3>
-										<p>{element.query}</p>
-									</div>
-									<div>
-										<h3 className="font-bold">
-											Arjun's GPT
-										</h3>
-										<p>{element.response}</p>
-									</div>
-								</div>
-							);
-						}
-					)}
-				</div>
-			</div>
-			<div className="h-[10vh] bg-gray-100">
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						e.stopPropagation();
-						getGPTresponse(e);
-					}}
-				>
-					<div>
-						<input
-							className="border border-black w-[65vw] mx-[5vw] mb-8 rounded-md py-4 px-2 absolute bottom-0 pr-[5vw]"
-							type="text"
-							name="prompt"
-							id="promptInput"
-							placeholder="Ask a question..."
-							onChange={(e) => setInputPrompt(e.target.value)}
-						/>
-						<button
-							className="z-20 mx-[5vw] mb-8 rounded-md py-4 px-2 absolute bottom-0 right-0"
-							type="button"
-							id=""
-							onClick={getGPTresponse}
-						>
-							Submit
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div id="chat" className="bg-gray-100 w-full h-[90vh] overflow-y-scroll">
+        <div className="w-[70%] ml-[10%] mt-8 space-y-4 mb-[50vh]">
+          {messgs.map(
+            (element: { id: string; query: string; response: string }) => {
+              return (
+                <div key={element.id}>
+                  <div>
+                    <h3 className="font-bold">You</h3>
+                    <p>{element.query}</p>
+                  </div>
+                  <div>
+                    <h3 className="font-bold">Chef GPT</h3>
+                    <p className="whitespace-pre-line">{element.response}</p>
+                  </div>
+                </div>
+              );
+            }
+          )}
+        </div>
+      </div>
+      <div className="h-[10vh] bg-gray-100">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            getGPTresponse(e);
+          }}
+        >
+          <div>
+            <input
+              className="border border-black w-[65vw] mx-[5vw] mb-8 rounded-md py-4 px-2 absolute bottom-0 pr-[5vw]"
+              type="text"
+              name="prompt"
+              id="promptInput"
+              placeholder="Ask a question..."
+              onChange={(e) => setInputPrompt(e.target.value)}
+            />
+            <button
+              className="z-20 mx-[5vw] mb-8 rounded-md py-4 px-2 absolute bottom-0 right-0"
+              type="button"
+              id=""
+              onClick={getGPTresponse}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
